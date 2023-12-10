7d6baa445dea413d1b572c3bd23c62417f88f2e8
MianAnswer <miananswer@hotmail.com>
2023-12-09 22:21:14 -0500

    Make Goals Controller functions async

--- api/src/controllers/goals.controllers.ts
+++ api/src/controllers/goals.controllers.ts
@@ -1,29 +1,29 @@
 import { Request, Response } from 'express'
 import goalsService from '../services/goals.service'
 
-export const createGoal = (req: Request, res: Response) => {
+export const createGoal = async (req: Request, res: Response) => {
   const result = goalsService.createGoal()
   res.send(result)
 }
 
-export const deleteGoal = (req: Request, res: Response) => {
+export const deleteGoal = async (req: Request, res: Response) => {
   const { id } = req.params
   const result = goalsService.deleteGoal(Number(id))
   res.send(result)
 }
 
-export const getGoals = (req: Request, res: Response) => {
+export const getGoals = async (req: Request, res: Response) => {
   const result = goalsService.getGoals()
   res.send(result)
 }
 
-export const getGoal = (req: Request, res: Response) => {
+export const getGoal = async (req: Request, res: Response) => {
   const { id } = req.params
   const result = goalsService.getGoal(Number(id))
   res.send(result)
 }
 
-export const updateGoal = (req: Request, res: Response) => {
+export const updateGoal = async (req: Request, res: Response) => {
   const { id } = req.params
   const result = goalsService.updateGoal(Number(id))
   res.send(result)
